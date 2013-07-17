70bdcd6da788df3947ab27d5c49607fe95f7f4b7
Srivallabh <srivallabhbk@gmail.com>
2013-07-17 15:48:53 +0530

    Update preprocessing.py

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -2,7 +2,7 @@ from nltk import stem
 import sys
 import re
  
-#Casefolding is done in stemming itself.
+
 #stopwords_reference is a file which contains the stop words collection.
 stopwords=open('stopwords_reference','r+').read()
 
@@ -10,7 +10,7 @@ stopwords=open('stopwords_reference','r+').read()
 stemmer=stem.snowball.EnglishStemmer()
 f=open('dataset','r+')
 text_input=f.read()
-#while(len(text_input)>0):
+
 for word in text_input.split(" "):
 	word.lower()
 	if(stopwords.find(word)<0):

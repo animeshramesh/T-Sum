184c2b94fe993feb0d4a32625a7b444c728a45fa
animeshramesh <animeshramesh@gmail.com>
2013-07-17 21:20:46 +0530

    Update preprocessing.py

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -13,11 +13,11 @@ text_input=open('dataset','r+').read()
 stemmer=stem.snowball.EnglishStemmer()
 
 
-for word in text_input.split(" "):	#Iterating through the words in the dataset
-	word.lower()	# Implementing case-folding
-	if(stopwords.find(word)<0):	# If word isnt a part of stopword collection
+for word in text_input.split(" "):	
+	word.lower()	
+	if(stopwords.find(word)<0):	
 			word=re.sub(r"[^\w\s]",'',word)	  
-			sys.stdout.write(stemmer.stem(word)+' '); # Stem and print the word
+			sys.stdout.write(stemmer.stem(word)+' '); 
 
 
 # Preprocessing ends

273cea416f8555ee377e3a131633920bccd0b3df
srivallabh <vallabhcubs@gmail.com>
2013-07-18 23:12:22 +0530

    Code inspection

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -6,18 +6,18 @@ class preprocessor:
     __text = []        # Private variable
     
     
-    def preprocessor1(self):
+    def return_stop_words(self):
         stopwords=open('stopwords_reference','r+').read()
         return stopwords
     
     
     def stop_word_eliminate(self,data):
-	        
-	filtered_data = []
+            
+        filtered_data = []
         c = preprocessor()
-        stop_words = c.preprocessor1()
-        preprocessor.text = data.split()
-        for word in preprocessor.text:
+        stop_words = c.return_stop_words()
+        text = data.split()
+        for word in text:
             if (stop_words.find(word)<0):
               word=re.sub(r"[^\w\s]",'',word) 
               filtered_data.append(word)
@@ -28,46 +28,47 @@ class preprocessor:
     
     def stem_word(self,data):
       stemmer=stem.snowball.EnglishStemmer()
-      stemmed_data=[]
+      stemmed_data= " "
       for word in data:
-          stemmed_data.append(stemmer.stem(word))
-		
+         stemmed_data += stemmer.stem(word)+' '        
       return stemmed_data
     
     def to_lower_case(self,data):
-	
         return data.lower()
-       
     
-        
-        
-   
+    
+#The code from here,has just been used for testing;Suitable packages will be created. 
 class input_this:
     
-    
-    
-    def input_from_file(self):
-        input_this.text = open('dataset','r+').read()
+    def input_from_file(self,input_file):
+        input_this.text = open(input_file,'r+').read()
         return input_this.text
     
 class output_this:
     
-    
-
     def display(self,word):
         print (word)
         
         
+def main():
+	a = input_this()
+	b = output_this()
+	prep = preprocessor()
+
+	inputdataset = a.input_from_file("dataset");
+
+	filter1 = prep.to_lower_case(inputdataset)
+		
+	filter2 = prep.stop_word_eliminate(filter1)
+			
+	filter3 = prep.stem_word(filter2)
+	b.display(filter3)     
+
+main()
 
 
-a = input_this()
-b = output_this()
-prep = preprocessor()
 
-inputdataset = a.input_from_file();
-filter1 = prep.to_lower_case(inputdataset)
-filter2 = prep.stop_word_eliminate(filter1)
-filter3 = prep.stem_word(filter2)
-print filter3
+
+
 
 

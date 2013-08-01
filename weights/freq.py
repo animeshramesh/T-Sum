7def541eab67ee03c18ad6c1e0ec4a4000d8b876
animeshramesh <animeshramesh@gmail.com>
2013-08-01 20:17:36 +0530

    Set doesnt allow indexing.

--- weights/freq.py
+++ weights/freq.py
@@ -20,6 +20,7 @@ def main():
     prep1 = preprocessor()
     i = 1
     inputdataset = ""
+    all_features = ""
     while 1:
         try:
             with open('%d.txt' % i):
@@ -27,10 +28,22 @@ def main():
                 filter1 = prep1.to_lower_case(inputdataset)
                 filter2 = prep1.stop_word_eliminate(filter1)
                 filter3 = prep1.stem_word(filter2)
-                i += 1
-                b.display(filter3)
+                all_features+=filter3 + ' '
                 print '\n'*2
+                i += 1
         except IOError:
             break
+    print all_features
+    features = set(all_features)
+    #print features
+    tot_freq = []
+    no_of_files = i-1
+    '''for j in range (0,len(features)-1):
+        tot_freq.append(all_features.count(features[j]))
+        i+=1
+    print tot_freq'''
+
+
+
 
 main()

912c0e7bdfdc0f47a5fc6327454d5b4af4d07a40
Pramod N <npramod05@gmail.com>
2013-08-03 19:26:18 +0530

    [Structure Change] Fixed package issues; moved preprocessor to be part of utils

---
+++ utils/preprocessor.py
@@ -0,0 +1,36 @@
+import re,os
+from nltk import stem
+
+class Preprocessor:
+
+    __text = []        # Private variable
+
+    def return_stop_words(self):
+        stop_words_filename = os.path.join(os.path.dirname(__file__),'stopwords_reference')
+        stopwords = open(stop_words_filename,'r+').read()
+        return stopwords
+
+    def stop_word_eliminate(self,data):
+        filtered_data = []
+        stop_words = self.return_stop_words()
+        text = data.split()
+        for word in text:
+            if (stop_words.find(word)<0):
+                word=re.sub(r"[^\w\s]",'',word) 
+                filtered_data.append(word)
+        return filtered_data
+
+
+    def stem_word(self,data):
+        stemmer=stem.snowball.EnglishStemmer()
+        stemmed_data= " "
+        for word in data:
+            stemmed_data += stemmer.stem(word)+' '
+        return stemmed_data
+
+    def to_lower_case(self,data):
+        return data.lower()
+
+
+
+

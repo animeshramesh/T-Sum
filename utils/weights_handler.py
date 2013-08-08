4ae5d84e7a0b8a148c54c9b4cd565b2f942c6b70
Animesh <animeshramesh@gmail.com>
2013-08-08 10:56:43 +0530

    Implemented dictionaries

--- utils/weights_handler.py
+++ utils/weights_handler.py
@@ -2,52 +2,45 @@ from utils.file_reader import FileReader
 from sys import stdout
 from math import log
 from utils.preprocessor import Preprocessor
-from utils.feature_handler import FeatureHandler
+from utils.feature_extractor import FeatureExtractor
 
 
 class WeightsHandler:
 
-    __tot_frequency = []
-    __term_freq_matrix = []
-    __inverse_document_freq = []
-    __tot_words_in_each_doc = []
-    __TFIDF = []
-
-    def tot_frequency(self):
-        return self.__tot_freq
-
-    def generate_total_frequency(self, unique_features, all_features):
-        for word in unique_features:
-            self.__tot_frequency.append(all_features.count(word))
-
-    def generate_term_freq_matrix(self, number):
-        for i in range(1, number):
-            file_data = input_handler.input_from_file('out%i.txt' % i)
-            arr = []
-            for each_word in Feature_Handler.feature_handler.unique_features:
-                arr.append(file_data.count(each_word))
-            self.__term_freq_matrix.append(arr)
-
-    def term_freq_matrix(self):
-        return self.__term_freq_matrix
-
-    def generate_inverse_document_freq(self, total_docs, unique_features):
-        for each_word in Feature_Handler.feature_handler.unique_features:
-            docs = 0
-            for i in range(1, total_docs):
-                input_data = input_handler.input_from_file('out%i.txt' % i)
-                if input_data.count(each_word) > 0:
-                    docs += 1
-            self.__inverse_document_freq.append(docs)
-
-    def inverse_document_freq(self):
-        return self.__inverse_document_freq
-
-    def generate_tot_features_in_each_doc(self, tot_files):
-        for i in range(1, tot_files):
-            input_data = input_handler.input_from_file('out%i.txt' % i)
-            self.__tot_words_in_each_doc.append(len(input_data.split()))
+    
+    __tot_freq_dict = {}		
+    __inverse_doc_freq_dict = {}	
+   
+    def tot_freq_dict(self):
+        return self.__tot_freq_dict
+  
+    def inverse_doc_freq(self):
+	return self.__inverse_doc_freq_dict
 
     
+    def update_totfreq_dict(self, key):
+	if key in self.__tot_freq_dict:
+	   self.__tot_freq_dict[key] += 1
+	else:
+	   self.__tot_freq_dict[key] = 1
+  
+    def generate_inv_doc_freq_dict(self, total_docs):
+	for each_feature in self.__tot_term_freq_dict.keys():
+	    docs = 0
+	    for i in range(total_docs):
+                input_data = FileReader.read('out%i.txt' % i)
+		if input_data.count(each_feature) > 0:
+                    docs += 1
+	    self.__inverse_doc_freq_dict[each_feature] = docs
+	
+
+    def read(self):
+        inputdataset = open('1.txt', 'r+').read().split()
+	for i in range(0, len(inputdataset)):
+	    self.update_totfreq_dict(inputdataset[i])
+	    
+	
+	
+	
 
 

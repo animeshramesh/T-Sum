48192f9fef9b79131c0f525e42b9f5eb3e2a3b2a
animeshramesh <animeshramesh@gmail.com>
2013-08-08 12:13:21 +0530

    Delete feature_reducer.py

--- utils/feature_reducer.py
+++
@@ -1,7 +0,0 @@
-class FeatureReducer:
-
-    def reduceFeatures(self, TotTermFreq_Dictionary):
-        for each_feature in range(len(TotTermFreq_Dictionary.keys())):
-	    if TotTermFreq_Dictionary[each_feature] = 1:
-	        del TotTermFreq_Dictionary[each_feature]
-	return TotTermFreq_Dictionary

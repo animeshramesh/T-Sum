952eec33ca4130c73bd61107ea2c99998193ab1e
Animesh <animeshramesh@gmail.com>
2013-08-08 12:39:37 +0530

    Feature Reduction Working

---
+++ utils/feature_reducer.py
@@ -0,0 +1,8 @@
+class FeatureReducer:
+
+    def reduceFeatures(self, TermFreq_Dictionary):   
+	for key,value in TermFreq_Dictionary.items():
+	    if value == 1:
+                del TermFreq_Dictionary[key]
+
+	return d

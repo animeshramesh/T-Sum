45bded4f72d836847c854c4b56b1ddd7a91215ba
Animesh <animeshramesh@gmail.com>
2013-08-08 11:57:37 +0530

    Feature Reduction implemented

--- utils/__init__.py
+++ utils/__init__.py
@@ -4,3 +4,4 @@ from utils.file_reader import FileReader
 from utils.file_writer import FileWriter
 from utils.preprocessor import Preprocessor
 from utils.feature_extractor import FeatureExtractor
+from utils.feature_reducer import FeatureReducer
---
+++ utils/feature_reducer.py
@@ -0,0 +1,7 @@
+class FeatureReducer:
+
+    def reduceFeatures(self, TotTermFreq_Dictionary):
+        for each_feature in range(len(TotTermFreq_Dictionary.keys())):
+	    if TotTermFreq_Dictionary[each_feature] = 1:
+	        del TotTermFreq_Dictionary[each_feature]
+	return TotTermFreq_Dictionary

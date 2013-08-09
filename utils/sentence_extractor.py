4f80c3814b8b2804b5f4f655df42d84c2e35a583
animeshramesh <animeshramesh@gmail.com>
2013-08-09 10:03:40 +0530

    Create sentence_extractor.py

---
+++ utils/sentence_extractor.py
@@ -0,0 +1,9 @@
+import re
+
+class SentenceExtractor:
+    
+    def extract_sentences(self, textInDocument):
+        sentenceEnders = re.compile('[?!.]')
+  sentenceList = sentenceEnders.split(textInDocument)
+	newSentenceList = [sentence for sentence in sentenceList if len(sentence) > 3]
+	return newSentenceList

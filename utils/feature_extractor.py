3b497f2e9d1ce16da6fa2af6b571ef6ff236e356
Animesh <animeshramesh@gmail.com>
2013-08-10 14:26:30 +0530

    Preprocessing word-wise

--- utils/feature_extractor.py
+++ utils/feature_extractor.py
@@ -13,29 +13,32 @@ from os import path
 class FeatureExtractor:
 
 
-    
+
     __total_documents = 0
 
     def extract(self, dataset_directory):
+        #This function is doing too much stuff. Need to separate the tasks.
+
         dataset_Reader = FileReader()
         dataset_preprocessor = Preprocessor()
-	dataset_WeightsHandler = WeightsHandler()
+    	dataset_WeightsHandler = WeightsHandler()
         i = 1
         while 1:
             try:
                 with open(dataset_directory + '/%d.txt' % i):
                     inputdataset = dataset_Reader.read(dataset_directory + '/%d.txt' % i)
-		    dataset_WeightsHandler.update_sentenceList(inputdataset)
-		    preprocessed_data = dataset_preprocessor.preprocess(inputdataset)
-                    for j in range(len(preprocessed_data.split())):
-			dataset_WeightsHandler.update_totfreq_dict(preprocessed_data.split()[j])
+	    			dataset_WeightsHandler.update_sentenceList(inputdataset)
+                    for word in inputdataset.split():
+                        preprocessed_data.append(dataset_preprocessor.preprocess(inputdataset.split()[k]))
+                    for j in range(len(preprocessed_data)):
+	            		dataset_WeightsHandler.update_totfreq_dict(preprocessed_data[j])
                     i += 1
             except IOError:
                 break
-  	
+
 	self.__total_documents = i-1
-	
-   
+
+
     def total_documents(self):
         return self.__total_documents
 

d01a23156c4dcfd4fda154097f576fd5b6f49ec1
Animesh <animeshramesh@gmail.com>
2013-08-17 13:26:39 +0530

    Refactored summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -6,39 +6,57 @@ from utils.preprocessor import Preprocessor
 from utils.sentence_extractor import SentenceExtractor
 from utils.feature_reducer import FeatureReducer
 from utils.weights_handler import WeightsHandler
+import os
 
-tot_docs = 0
+class Summarizer:
+	
+	def summarize(self,input_path):
+		
+		tot_docs = 0
+		preprocessed_list = []
+		dataset_Reader = FileReader()
+		dataset_output_handler = FileWriter()
+		dataset_preprocessor = Preprocessor()
+		dataset_WeightsHandler = WeightsHandler()
+		dataset_FeatureReducer = FeatureReducer()
+		
+		
+		files = [f for f in os.listdir(input_path) if os.path.isfile(input_path + '/'+ f)]
+		
+		try:
+			for doc in files:
+				with open(input_path + '/' + doc):
+					print input_path + '/' + doc
+					inputdataset = dataset_Reader.read(input_path + '/' + doc)
+					
+					dataset_WeightsHandler.update_sentenceList(inputdataset)  
+					
+					preprocessed_data = []
+					for word in inputdataset.split():
+						word = word.lower()
+						if word not in dataset_preprocessor.stop_words():
+							preprocessed_data.append(str(dataset_preprocessor.preprocess(word)))
+  		
+					for j in range(len(preprocessed_data)):
+						dataset_WeightsHandler.update_totfreq_dict(preprocessed_data[j])
+					preprocessed_list.append(preprocessed_data)
+	
 
-dataset_Reader = FileReader()
-dataset_output_handler = FileWriter()
-dataset_preprocessor = Preprocessor()
-dataset_WeightsHandler = WeightsHandler()
-dataset_FeatureReducer = FeatureReducer()
-dataset_directory = raw_input()
-i = 1
-while 1:
-    try:
-        with open(dataset_directory + '%d.txt' % i):
-            inputdataset = dataset_Reader.read(dataset_directory + '%d.txt' % i)
-            dataset_WeightsHandler.update_sentenceList(inputdataset)
-            preprocessed_data = []
-            for word in inputdataset.split():
-                word = word.lower()
-                if word not in dataset_preprocessor.stop_words():
-                    preprocessed_data.append(str(dataset_preprocessor.preprocess(word)))
-                # To lower and stop-word elimination above required for removing 'none' in dictionary
-            dataset_output_handler.write(dataset_directory + 'out%d.txt' % i, preprocessed_data)
-            for j in range(len(preprocessed_data)):
-                dataset_WeightsHandler.update_totfreq_dict(preprocessed_data[j])
-            i += 1
+		except IOError:
+			print "IOError"
 
-    except IOError:
-        break
+		print preprocessed_list
+		dataset_WeightsHandler.replace_totfreq_dict(dataset_FeatureReducer.reduceFeatures(dataset_WeightsHandler.tot_freq_dict()))
+		
+		#dataset_WeightsHandler.generate_inv_doc_freq_dict(total_documents, input_path)
+		#dataset_WeightsHandler.generate_tot_weight_dict()
+		dataset_WeightsHandler.generate_STM()
+		#print dataset_WeightsHandler.sentenceWeight_dict()
 
-total_documents = i - 1
-dataset_WeightsHandler.replace_totfreq_dict(dataset_FeatureReducer.reduceFeatures(dataset_WeightsHandler.tot_freq_dict()))
-dataset_WeightsHandler.generate_inv_doc_freq_dict(total_documents, dataset_directory)
-dataset_WeightsHandler.generate_tot_weight_dict()
-dataset_WeightsHandler.generate_STM()
-print dataset_WeightsHandler.sentenceWeight_dict()
 
+
+if __name__ == '__main__':
+	import sys	
+	input_path = sys.argv[1]
+	summarizer = Summarizer()
+	summarizer.summarize(input_path)

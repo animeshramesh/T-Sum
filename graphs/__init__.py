7d74f9bb7a513a1217101751ecf921415841ceed
Animesh <animeshramesh@gmail.com>
2013-08-27 08:19:04 +0530

    Working on word similarity score

--- graphs/__init__.py
+++ graphs/__init__.py
@@ -1,2 +1,3 @@
-from graphs.graph_generator import GraphGenerator
+from graphs.graph_generator import *
 from graphs.cosine_relation_extractor import CosineRelationExtractor
+from graphs.similarity_scorer import SimilarityScorer

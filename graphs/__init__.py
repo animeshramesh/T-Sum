8cfb0366d1f45ce5be6b622cca51f580d8be26a1
Animesh <animeshramesh@gmail.com>
2013-09-01 20:20:59 +0530

    Dissimilarity Matrix working

--- graphs/__init__.py
+++ graphs/__init__.py
@@ -1,3 +1,3 @@
 from graphs.graph_generator import *
 from graphs.cosine_relation_extractor import CosineRelationExtractor
-from graphs.similarity_scorer import SimilarityScorer
+from graphs.dissimilarity_scorer import DissimilarityScorer
---
+++ graphs/dissimilarity_scorer.py
@@ -0,0 +1,27 @@
+class DissimilarityScorer:
+    
+    def assign_dissimilarity_score(self, synonym_dict, sentence_list):
+        dissimilarity_matrix = []
+        for sentence1 in sentence_list:
+            sentence_dissimilarity = []
+            for sentence2 in sentence_list:
+                dissimilarity_score = 0
+                number_of_comparisons = len(sentence1.split()) * len(sentence2.split())
+                for word1 in sentence1.split():
+                    word1_synsets = synonym_dict[word1]
+                    for word2 in sentence2.split():
+                        word2_synsets = synonym_dict[word2]
+                        dissimilarity_score += (1 - self.max_wup_similarity(word1_synsets, word2_synsets))
+                sentence_dissimilarity.append(dissimilarity_score/number_of_comparisons)
+            dissimilarity_matrix.append(sentence_dissimilarity)
+        return dissimilarity_matrix
+                
+                        
+    def max_wup_similarity(self, word1_synsets, word2_synsets):
+        max_similarity = 0
+        for syns1 in word1_synsets:
+            for syns2 in word2_synsets:
+                similarity_score = syns1.wup_similarity(syns2)
+                if similarity_score > max_similarity:
+                    max_similarity = similarity_score
+        return max_similarity
